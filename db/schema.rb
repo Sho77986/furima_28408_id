8a30e825ae149fae2ea183a65cc176fed3951630
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-02 20:05:28 +0900

    ログインログアウト新規登録の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_061415) do
+ActiveRecord::Schema.define(version: 2020_08_01_071231) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
