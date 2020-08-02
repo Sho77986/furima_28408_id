aa993539f6e6821dd7961e7d1349ae5ee5ec38ed
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-02 21:28:17 +0900

    バリデーションの追加実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,28 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true
+
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true
+  validates :password, length:{ minimum: 6 }
+  validates :password, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, confirmation: true
+  validates :first_name, presence: true
+  validates :family_name, presence: true
+  validates :first_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
+  validates :family_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
+  validates :first_name_furigana, presence: true
+  validates :family_name_furigana, presence: true
+  validates :first_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
+  validates :family_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
+  validates :birthday, presence: true
+
   has_many :comments
   has_many :items
   has_many :purchases
-  
-  
+
 end
