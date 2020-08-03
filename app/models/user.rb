32f126e6cc5cd40e60200dbd2a8b131c12235472
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-03 14:54:29 +0900

    変数追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX =  /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i 
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ンー-龥]+\z/i
+  VALID_FURIGANA_REGEX =  /\A[ァ-ン]+\z/i 
+
   with_options presence: true do
     validates :nickname,  uniqueness: { case_sensitive: true }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-    validates :first_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
-    validates :family_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
-    validates :first_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
-    validates :family_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :first_name, format: {with: VALID_NAME_REGEX }
+    validates :family_name, format: {with: VALID_NAME_REGEX }
+    validates :first_name_furigana, format: {with: VALID_FURIGANA_REGEX } 
+    validates :family_name_furigana, format: {with: VALID_FURIGANA_REGEX }
     validates :birthday
   end
 
