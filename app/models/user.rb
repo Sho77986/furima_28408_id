4db9f3dd71ae05a590733d8b68337314f1ac027f
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-03 10:57:37 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,27 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :email, uniqueness: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true
-  validates :password, length:{ minimum: 6 }
-  validates :password, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, confirmation: true
-  validates :first_name, presence: true
-  validates :family_name, presence: true
-  validates :first_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
-  validates :family_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
-  validates :first_name_furigana, presence: true
-  validates :family_name_furigana, presence: true
-  validates :first_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
-  validates :family_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
-  validates :birthday, presence: true
 
-  has_many :comments
-  has_many :items
-  has_many :purchases
 
+  with_options presence: true do
+    validates :nickname,  uniqueness: { case_sensitive: true }
+    validates :email, uniqueness: true
+    validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    validates :password, length:{ minimum: 6 }
+    validates :password, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, confirmation: true
+    validates :first_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
+    validates :family_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/i }
+    validates :first_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
+    validates :family_name_furigana, format: {with: /\A[ァ-ン]+\z/i }
+    validates :birthday
+  end
+
+   has_many :comments
+   has_many :items
+   has_many :purchases
+  
 end
