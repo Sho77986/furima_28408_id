fbc4eda22f747603eb0b33cd970c762a5647e744
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-04 18:10:59 +0900

    送料を指定するJava(途中)

---
+++ app/javascript/new.js
@@ -0,0 +1,24 @@
+console.log("OK")
+window.addEventListener("keyup", function(){
+
+  var shippingPrice = document.getElementById("price").value
+  console.log(shippingPrice)
+
+   value 
+   price = Number(shippingPrice).val()
+  // shouhizei=price *0.1
+  // add-tax-price.text(shouhizeip)
+    // shippingPrice.addEventListener('keyup',function (){
+    //   var shippingCharge = document.getElementById("add-tax-price")
+    //    shippingCharge = price_input * 0.1
+
+    //   var shippingCharge = document.getElementById("profit")
+    //   return shippingProfit = price_input * 0.9
+       
+      
+ })
+
+// keyup
+// 入力した値を取得
+// 販売手数料と販売利益を計算
+// 計算した値を画面に表示
