3e02f0214216064352ff5892f1a9d4f1d06b0ff5
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-07 19:17:07 +0900

    rspec修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
   factory :item do
-    text {Faker::Lorem.sentence}
-    image {Faker::Lorem.sentence}
+    name                  {"zxcv"}
+    text                  {"qwertyuiopasdfghjkl"}
+    image                 {"zxcvbnm.png"}
+    category_id           {"0"}
+    item_status_id        {"0"}
+    shipping_area_id      {"0"}
+    shipping_charge_id    {"0"}
+    shipping_day_id       {"0"}
     association :user
   end
 end
