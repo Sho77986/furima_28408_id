9a13c55a8051e406b4a912678be3bc67e89b79fe
Sho77986 <68266590+Sho77986@users.noreply.github.com>
2020-08-07 15:57:02 +0900

    非同期通信実装

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,24 +1,27 @@
 console.log("OK")
 window.addEventListener("keyup", function(){
 
-  var shippingPrice = document.getElementById("price").value
-  console.log(shippingPrice)
 
-   value 
-   price = Number(shippingPrice).val()
-  // shouhizei=price *0.1
-  // add-tax-price.text(shouhizeip)
-    // shippingPrice.addEventListener('keyup',function (){
-    //   var shippingCharge = document.getElementById("add-tax-price")
-    //    shippingCharge = price_input * 0.1
+    var Price = document.getElementById("item_price").value
+     console.log(Price)
+   
+     var SalesCommission = document.getElementById('add-tax-price')
+     var SalesCommission = Price * 0.1;
+     var Profit = document.getElementById('profit')
+     var Profit = Price * 0.9;
 
-    //   var shippingCharge = document.getElementById("profit")
-    //   return shippingProfit = price_input * 0.9
-       
-      
+     console.log(SalesCommission)
+     console.log(Profit)
+
+     $('#add-tax-price').html(SalesCommission);
+     $('#profit').html(Profit); 
+
  })
 
 // keyup
 // 入力した値を取得
 // 販売手数料と販売利益を計算
 // 計算した値を画面に表示
+
+ //salesCommission = Number(SalesCommission).val();
+ //profit = Number(Profit).val();
